4e7a1d771679994aaaedd5fcdf3d3b2296edee1b
IAmJamesHall <thatwhichendureth@gmail.com>
2020-06-09 12:37:07 -0400

    Fixed routing issue

--- app.js
+++ app.js
@@ -1,48 +1,43 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const routes = require('./routes/index');
-const books = require('./routes/books');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const routes = require("./routes/index");
+const books = require("./routes/books");
 
 const app = express();
-const path = require('path');
-
-
-
+const path = require("path");
 
 // view engine (pug) setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // cookie parser
 app.use(cookieParser());
 
 //process incoming POST requests
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-
 //set dir for static files
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
-app.use('/books', books);
+app.use("/", routes);
+app.use("/books", books);
 
 // catch 404s
 app.use((req, res, next) => {
-  const error = new Error('404: not found');
-  next(error)
-})
+  const error = new Error("404 - not found");
+  next(error);
+});
 
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-   // render the error page
-   res.status(err.status || 500);
-   console.log(res.locals.error)
-   res.render('error', { err });
-})
+  // render the error page
+  res.status(err.status || 500);
+  console.log(res.locals.error);
+  res.render("error", { err });
+});
 
 module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -1,168 +1,188 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Op } = require('sequelize');
-const { Book } = require('../models');
+const { Op } = require("sequelize");
+const { Book } = require("../models");
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb) {
   return async (req, res, next) => {
     try {
-      await cb(req, res, next)
+      await cb(req, res, next);
     } catch (error) {
       console.log(error);
       res.status(500).send(error);
     }
-  }
+  };
 }
 
 /* GET listing of books */
-router.get('/', asyncHandler(async (req, res) => {
-  // if no cookies, create an empty object
-  if (!req.cookies) {
-    req.cookies = {};
-  }
-
-  
-
-  // URL querys that are also stored in cookies to save state
-  const sortBy = req.query.sortBy || req.cookies.sortBy || "title";
-  const direction = req.query.direction || req.cookies.direction || "ASC";
-  const searchQuery = req.query.searchQuery || req.cookies.searchQuery || "";
-  
-  // boilerplate table headings
-  let headers = {
-    title: {direction: 'ASC'},
-    author: {direction: 'ASC'},
-    genre: {direction: 'ASC'},
-    year: {direction: 'ASC'}
-  }
-
-  // depending on the current query, table headings will be changed accordingly
-  // if current direction is ASC, change the 'direction' property of the currently
-  // sorted table so that if it is clicked again, it will be DESC.
-  // Also, toggle the direction of the arrows
-  if (direction === "ASC") {
-    headers[sortBy].direction = "DESC";
-    headers[sortBy].arrow = "up";
-  } else if (direction === "DESC") {
-    headers[sortBy].arrow = "down";
-  }
-
-  // find all books that match the query and return the results in the correct order
-  const books = await Book.findAll({
-    order: [[sortBy, direction]],
-    where: {
-      [Op.or]: [
-        {
-          title: {
-            [Op.like]: `%${searchQuery}%`
-          }
-        },
-        {
-          author: {
-            [Op.like]: `%${searchQuery}%`
-          }
-        },
-        {
-          genre: {
-            [Op.like]: `%${searchQuery}%`
-          }
-        },
-        {
-          year: {
-            [Op.like]: `%${searchQuery}%`
-          }
-        },
-      ]
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    // if no cookies, create an empty object
+    if (!req.cookies) {
+      req.cookies = {};
     }
-  });
 
-  // place cookies for the current query items to save state
-  res.cookie('sortBy', sortBy);
-  res.cookie('direction', direction);
-  res.cookie('searchQuery', searchQuery);
+    // URL querys that are also stored in cookies to save state
+    const sortBy = req.query.sortBy || req.cookies.sortBy || "title";
+    const direction = req.query.direction || req.cookies.direction || "ASC";
+    const searchQuery = req.query.searchQuery || req.cookies.searchQuery || "";
 
-  res.render('books/all_books', {
-    title: "Books", books, headers, sortBy, searchQuery });
-}));
+    // boilerplate table headings
+    let headers = {
+      title: { direction: "ASC" },
+      author: { direction: "ASC" },
+      genre: { direction: "ASC" },
+      year: { direction: "ASC" },
+    };
+
+    // depending on the current query, table headings will be changed accordingly
+    // if current direction is ASC, change the 'direction' property of the currently
+    // sorted table so that if it is clicked again, it will be DESC.
+    // Also, toggle the direction of the arrows
+    if (direction === "ASC") {
+      headers[sortBy].direction = "DESC";
+      headers[sortBy].arrow = "up";
+    } else if (direction === "DESC") {
+      headers[sortBy].arrow = "down";
+    }
+
+    // find all books that match the query and return the results in the correct order
+    const books = await Book.findAll({
+      order: [[sortBy, direction]],
+      where: {
+        [Op.or]: [
+          {
+            title: {
+              [Op.like]: `%${searchQuery}%`,
+            },
+          },
+          {
+            author: {
+              [Op.like]: `%${searchQuery}%`,
+            },
+          },
+          {
+            genre: {
+              [Op.like]: `%${searchQuery}%`,
+            },
+          },
+          {
+            year: {
+              [Op.like]: `%${searchQuery}%`,
+            },
+          },
+        ],
+      },
+    });
+
+    // place cookies for the current query items to save state
+    res.cookie("sortBy", sortBy);
+    res.cookie("direction", direction);
+    res.cookie("searchQuery", searchQuery);
+
+    res.render("books/all_books", {
+      title: "Books",
+      books,
+      headers,
+      sortBy,
+      searchQuery,
+    });
+  })
+);
 
 /* Clear search form */
-router.get('/clearSearch', (req, res) => {
-  res.clearCookie('searchQuery');
-  res.redirect('/books');
-})
-
-
-
+router.get("/clearSearch", (req, res) => {
+  res.clearCookie("searchQuery");
+  res.redirect("/books");
+});
 
 /* GET new book form */
-router.get('/new', asyncHandler(async (req, res) => {
-  res.render('books/new_book', { title: "New Book", book: {}})
-}));
-
+router.get(
+  "/new",
+  asyncHandler(async (req, res) => {
+    res.render("books/new_book", { title: "New Book", book: {} });
+  })
+);
 
 /* POST new book to database */
-router.post('/new', asyncHandler(async (req, res) => {
-  let book;
-  try {
-    book = await Book.create(req.body);
-    res.redirect('/books');
-  } catch (error) {
-    if (error.name === "SequelizeValidationError") { //if not all fields are valid
-      console.log('seqvalErr')
-      book = await Book.build(req.body);
-      console.log(book.toJSON());
-      res.render('books/new_book', { book: book.toJSON(), errors: error.errors })
-    } else {
-      throw error
+router.post(
+  "/new",
+  asyncHandler(async (req, res) => {
+    let book;
+    try {
+      book = await Book.create(req.body);
+      res.redirect("/books");
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        //if not all fields are valid
+        console.log("seqvalErr");
+        book = await Book.build(req.body);
+        console.log(book.toJSON());
+        res.render("books/new_book", {
+          book: book.toJSON(),
+          errors: error.errors,
+        });
+      } else {
+        throw error;
+      }
     }
-  }
-}));
-
+  })
+);
 
 /* GET info for individual book */
-router.get('/:id', asyncHandler(async (req, res) => {
-  const book = await Book.findByPk(req.params.id);
-  if (!book) {
-    res.redirect('/books');
-  } else {
-    res.render("books/book_detail", { book });
-  }
-
-}));
-
+router.get(
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const book = await Book.findByPk(req.params.id);
+    if (!book) {
+      const error = new Error("404 - not found");
+      next(error);
+      res.redirect("/books");
+    } else {
+      res.render("books/book_detail", { book });
+    }
+  })
+);
 
 /* POST update for individual book */
-router.post('/:id', asyncHandler(async (req, res) => {
-  let book;
-  try {
-    book = await Book.findByPk(req.params.id)
-    if (book) {
-      await book.update(req.body);
-      res.redirect('/books')
-    } else {
-      res.status(404);
+router.post(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    let book;
+    try {
+      book = await Book.findByPk(req.params.id);
+      if (book) {
+        await book.update(req.body);
+        res.redirect("/books");
+      } else {
+        res.status(404);
+      }
+    } catch (error) {
+      if (error.name === "SequelizeValidationError") {
+        console.log("SeqValErr");
+        book = await Book.build(req.body).toJSON();
+        book.id = req.params.id;
+        console.log(error.errors);
+        res.render("books/book_detail", { book, errors: error.errors });
+      }
     }
-  } catch (error) {
-    if (error.name === "SequelizeValidationError") {
-      console.log('SeqValErr');
-      book = await Book.build(req.body).toJSON();
-      book.id = req.params.id;
-      console.log(error.errors);
-      res.render('books/book_detail', { book, errors: error.errors })
-    }
-  }
-}));
+  })
+);
 
 /* POST a delete request for an individual book */
-router.post('/:id/delete', asyncHandler(async (req, res) => {
-  const book = await Book.findByPk(req.params.id);
-  if (book) {
-    await book.destroy();
-    res.redirect('/books');
-  } else {
-    res.sendStatus(404);
-  }
-}));
+router.post(
+  "/:id/delete",
+  asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.destroy();
+      res.redirect("/books");
+    } else {
+      res.sendStatus(404);
+    }
+  })
+);
 
 module.exports = router;
